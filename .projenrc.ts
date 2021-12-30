a03c7cd1fd41e508b704279c460ec0468ea39ac0
Roman Filippov <rf@romanfilippov.com>
2021-12-30 16:58:07 +0700

    chore: removes stale comment code

--- .projenrc.ts
+++ .projenrc.ts
@@ -13,7 +13,6 @@ const project = new typescript.TypeScriptProject({
   ],
 
 
-  // deps: [],                /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
   // devDeps: [],             /* Build dependencies for this module. */
   // packageName: undefined,  /* The "name" in package.json. */
