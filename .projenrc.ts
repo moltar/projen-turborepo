a4c256cbf4f7eb89f72062347722055b810ad674
Roman Filippov <rf@romanfilippov.com>
2021-12-30 17:25:29 +0700

    chore: adds MIT license

--- .projenrc.ts
+++ .projenrc.ts
@@ -3,6 +3,8 @@ import { typescript, javascript } from 'projen'
 const project = new typescript.TypeScriptProject({
   defaultReleaseBranch: 'master',
   name: 'projen-turborepo',
+  license: 'MIT',
+  copyrightOwner: 'Roman Filippov',
   projenrcTs: true,
   packageManager: javascript.NodePackageManager.NPM,
   deps: [
