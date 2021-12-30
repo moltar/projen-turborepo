80876b24a2cd1f5b21c01a9e4623f8e76acbb0a9
Roman Filippov <rf@romanfilippov.com>
2021-12-30 09:59:30 +0700

    chore: switch to npm

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,8 +1,11 @@
-import { typescript } from "projen";
+import { typescript } from 'projen';
+import { NodePackageManager } from 'projen/lib/javascript';
+
 const project = new typescript.TypeScriptProject({
-  defaultReleaseBranch: "main",
-  name: "projen-turborepo",
+  defaultReleaseBranch: 'main',
+  name: 'projen-turborepo',
   projenrcTs: true,
+  packageManager: NodePackageManager.NPM,
 
   // deps: [],                /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
