15cff1ddd69a56ded6371ea3c117df63024c9e46
Roman Filippov <rf@romanfilippov.com>
2021-12-30 17:28:39 +0700

    docs: adds package keywords

--- .projenrc.ts
+++ .projenrc.ts
@@ -4,6 +4,13 @@ const project = new typescript.TypeScriptProject({
   defaultReleaseBranch: 'master',
   name: 'projen-turborepo',
   description: 'Projen project type for Turborepo monorepo setup.',
+  keywords: [
+    'projen',
+    'turborepo',
+    'turbo',
+    'monorepo',
+    'typescript',
+  ],
   license: 'MIT',
   copyrightOwner: 'Roman Filippov',
   projenrcTs: true,
