b5b4d2dda4c1de16edab7ee6e06709e324a5b1a1
Roman Filippov <rf@romanfilippov.com>
2021-12-30 10:03:04 +0700

    chore: do not import internals

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,11 +1,10 @@
-import { typescript } from 'projen';
-import { NodePackageManager } from 'projen/lib/javascript';
+import { typescript, javascript } from 'projen';
 
 const project = new typescript.TypeScriptProject({
   defaultReleaseBranch: 'master',
   name: 'projen-turborepo',
   projenrcTs: true,
-  packageManager: NodePackageManager.NPM,
+  packageManager: javascript.NodePackageManager.NPM,
 
   // deps: [],                /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
