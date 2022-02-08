f41dddfe1186e09f95e245ba3f163670d0ffcbc2
Roman Filippov <rf@romanfilippov.com>
2022-02-08 09:03:44 +0700

    test: removes an outdated test

--- test/sub-projects.test.ts
+++ test/sub-projects.test.ts
@@ -154,35 +154,4 @@ describe('TurborepoProject', () => {
 
     expect(synth['.github/workflows/build.yml']).toMatchSnapshot()
   })
-
-  describe('for each sub-project', () => {
-    const project = createProject({
-      parallelWorkflows: true,
-      mutableBuild: false,
-    })
-
-    const subProjectBarDir = 'packages/bar'
-    const subProjectBar = createSubProject({
-      name: '@scoped/a-really-really-long-package-name-bar',
-      parent: project,
-      outdir: subProjectBarDir,
-    })
-
-    const subProjectBazDir = 'packages/baz'
-    createSubProject({
-      name: 'baz',
-      parent: project,
-      outdir: subProjectBazDir,
-      deps: [subProjectBar.package.packageName],
-    })
-
-    const synth = synthProjectSnapshot(project)
-    const githubWorkflowFiles = Object.keys(synth).filter((file) => file.startsWith('.github/workflows/'))
-
-    it.each(githubWorkflowFiles)('%s should have a GitHub actions workflow', (githubWorkflowFile) => {
-      expect.assertions(1)
-
-      expect(synth[githubWorkflowFile]).toMatchSnapshot()
-    })
-  })
 })
